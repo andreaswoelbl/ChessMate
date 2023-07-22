701287dc4b65d49a6f8e0f60522e5d7c5b740ead
bensuppanz <benjaminsuppanz@gmail.com>
2023-07-22 22:28:52 +0200

    added CheatFunktion class

---
+++ app/src/main/java/com/game/chessmate/GameFiles/CheatFunktion.java
@@ -0,0 +1,35 @@
+package com.game.chessmate.GameFiles;
+import com.game.chessmate.GameActivity;
+import com.game.chessmate.GameFiles.CheatFunktion;
+import com.game.chessmate.GameFiles.ChessBoard;
+import com.game.chessmate.GameFiles.Networking.NetObjects.PlayerDataObject;
+
+public class CheatFunktion {
+    private static boolean cheatFunction;
+    private int playerWhiteWrongCheatReveal = 0;
+    private int playerBlackWrongCheatReveal = 0;
+
+    Player playerWhite = ChessBoard.getInstance().getPlayer1();
+    Player playerBlack = ChessBoard.getInstance().getPlayer2();
+
+
+    public  void playerDidCheat() {
+        Field startPosition = ChessBoard.getInstance().getStartPossition();
+        if (playerBlack.getCheatOn() && !ChessBoard.getInstance().getwasMoveLegal()) {
+            ChessBoard.getInstance().getMovedPiece().move(startPosition);
+        } else if (playerWhite.getCheatOn() && !ChessBoard.getInstance().getwasMoveLegal())
+            ChessBoard.getInstance().getMovedPiece().move(startPosition);
+    }
+
+    public  void playerDidNotCheat(){
+
+    }
+
+    public static void setCheatFunction(boolean cheatFunction) {
+        CheatFunktion.cheatFunction = cheatFunction;
+    }
+    public static boolean isCheatFunction() {
+        return cheatFunction;
+    }
+}
+}
