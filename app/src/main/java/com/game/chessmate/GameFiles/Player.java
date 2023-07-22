a323978017b806609221ac9fbf2403efbf05df85
bensuppanz <benjaminsuppanz@gmail.com>
2023-07-23 00:01:47 +0200

    added variable to save amount of times the cheat funktion was used wrongly

--- app/src/main/java/com/game/chessmate/GameFiles/Player.java
+++ app/src/main/java/com/game/chessmate/GameFiles/Player.java
@@ -17,8 +17,9 @@ public class Player {
     private Field lastSelectedField;
     private ChessPieceColour color;
     private boolean cheatOn;
-
     private float lightValue;
+    private boolean wasLeganMove;
+    private int timesCheatFunktionUsedWrongly;
 
     /**
      * Instantiates a new Player.
@@ -32,6 +33,8 @@ public class Player {
         this.lastSelectedField = null;
         this.color = color;
         this.cheatOn = false;
+        this.wasLeganMove = true;
+        this.timesCheatFunktionUsedWrongly = 0;
     }
 
 
@@ -154,7 +157,24 @@ public class Player {
         return lightValue;
     }
 
+
     public void setLightValue(float lightValue) {
         this.lightValue = lightValue;
+    }
+
+    public boolean getWasLeganMove() {
+        return wasLeganMove;
+    }
+
+    public void setWasLeganMove(boolean wasLeganMove) {
+        this.wasLeganMove = wasLeganMove;
+    }
+
+    public int getTimesCheatFunktionUsedWrongly() {
+        return timesCheatFunktionUsedWrongly;
+    }
+
+    public void setTimesCheatFunktionUsedWrongly(int timesCheatFunktionUsedWrongly) {
+        this.timesCheatFunktionUsedWrongly = timesCheatFunktionUsedWrongly;
     }
 }
