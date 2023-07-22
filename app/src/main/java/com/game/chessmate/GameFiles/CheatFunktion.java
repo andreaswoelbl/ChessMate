04d01b69a3316c09f5b4f0f73279318165cc2ff3
bensuppanz <benjaminsuppanz@gmail.com>
2023-07-22 22:41:48 +0200

    implemented playerDidNotCheat method and changed variable names

--- app/src/main/java/com/game/chessmate/GameFiles/CheatFunktion.java
+++ app/src/main/java/com/game/chessmate/GameFiles/CheatFunktion.java
@@ -1,33 +1,35 @@
 package com.game.chessmate.GameFiles;
-import com.game.chessmate.GameActivity;
-import com.game.chessmate.GameFiles.CheatFunktion;
-import com.game.chessmate.GameFiles.ChessBoard;
-import com.game.chessmate.GameFiles.Networking.NetObjects.PlayerDataObject;
 
 public class CheatFunktion {
     private static boolean cheatFunction;
-    private int playerWhiteWrongCheatReveal = 0;
-    private int playerBlackWrongCheatReveal = 0;
+    private int playerLocalWrongCheatReveal = 0;
+    private int playerEnemyWrongCheatReveal = 0;
 
-    Player playerWhite = ChessBoard.getInstance().getPlayer1();
-    Player playerBlack = ChessBoard.getInstance().getPlayer2();
+    Player playerLocal = ChessBoard.getInstance().getLocalPlayer();
+    Player playerEnemy = ChessBoard.getInstance().getEnemyPlayer();
 
 
-    public  void playerDidCheat() {
+    public void playerDidCheat() {
         Field startPosition = ChessBoard.getInstance().getStartPossition();
-        if (playerBlack.getCheatOn() && !ChessBoard.getInstance().getwasMoveLegal()) {
+        if (playerEnemy.getCheatOn() && !ChessBoard.getInstance().getwasMoveLegal()) {
             ChessBoard.getInstance().getMovedPiece().move(startPosition);
-        } else if (playerWhite.getCheatOn() && !ChessBoard.getInstance().getwasMoveLegal())
+        } else if (playerLocal.getCheatOn() && !ChessBoard.getInstance().getwasMoveLegal())
             ChessBoard.getInstance().getMovedPiece().move(startPosition);
     }
 
-    public  void playerDidNotCheat(){
-
+    public void playerDidNotCheat() {
+        if (playerEnemy.getCheatOn() && ChessBoard.getInstance().getwasMoveLegal()) {
+            playerLocalWrongCheatReveal++;
+        }
+        if (playerLocal.getCheatOn() && ChessBoard.getInstance().getwasMoveLegal()) {
+            playerEnemyWrongCheatReveal++;
+        }
     }
 
     public static void setCheatFunction(boolean cheatFunction) {
         CheatFunktion.cheatFunction = cheatFunction;
     }
+
     public static boolean isCheatFunction() {
         return cheatFunction;
     }
