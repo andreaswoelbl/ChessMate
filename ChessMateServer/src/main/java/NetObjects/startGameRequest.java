4de3851086f9efb9efd3677dbfa05f7c3af0a169
Tobias Wernig <towernig@edu.aau.at>
2023-07-23 12:47:17 +0200

    updated backend to support cheat function

---
+++ ChessMateServer/src/main/java/NetObjects/startGameRequest.java
@@ -0,0 +1,14 @@
+package NetObjects;
+
+public class startGameRequest {
+    String lobbycode;
+    public startGameRequest(){}
+
+    public String getLobbycode() {
+        return lobbycode;
+    }
+
+    public void setLobbycode(String lobbycode) {
+        this.lobbycode = lobbycode;
+    }
+}
