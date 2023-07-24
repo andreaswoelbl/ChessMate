c33b8f7262cffea2f5f373204fb57d02a28b989f
bensuppanz <benjaminsuppanz@gmail.com>
2023-07-24 19:39:06 +0200

    added file for startGameParameters

---
+++ app/src/main/java/com/game/chessmate/GameFiles/Networking/NetObjects/startGameParameters.java
@@ -0,0 +1,18 @@
+package com.game.chessmate.GameFiles.Networking.NetObjects;
+
+import com.game.chessmate.GameFiles.PlayingPieces.ChessPieceColour;
+
+public class startGameParameters {
+    ChessPieceColour initColour;
+
+    public startGameParameters() {
+    }
+
+    public ChessPieceColour getInitColour() {
+        return initColour;
+    }
+
+    public void setInitColour(ChessPieceColour initColour) {
+        this.initColour = initColour;
+    }
+}
