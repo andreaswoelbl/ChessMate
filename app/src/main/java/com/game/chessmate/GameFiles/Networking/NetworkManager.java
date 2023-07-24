4621d4bde10fa893c3a13c189bd502e043e1c1a8
bensuppanz <benjaminsuppanz@gmail.com>
2023-07-24 19:40:19 +0200

    adjusted NetworkManager to handle lobbycodes

--- app/src/main/java/com/game/chessmate/GameFiles/Networking/NetworkManager.java
+++ app/src/main/java/com/game/chessmate/GameFiles/Networking/NetworkManager.java
@@ -1,32 +1,40 @@
 package com.game.chessmate.GameFiles.Networking;
 
-import android.content.Intent;
 import android.util.Log;
 
 import com.game.chessmate.GameFiles.Networking.NetObjects.LobbyDataObject;
-import com.game.chessmate.Lobby;
+import com.game.chessmate.GameFiles.Networking.NetObjects.startGameParameters;
+import com.game.chessmate.GameFiles.PlayingPieces.ChessPieceColour;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-/** The NetworkManager class functions as the framework for networked interaction. */
+/**
+ * The NetworkManager class functions as the framework for networked interaction.
+ */
 public class NetworkManager {
-    private static final class InstanceHolder {static final NetworkManager INSTANCE = new NetworkManager();}
-    public static NetworkManager getInstance(){ return NetworkManager.InstanceHolder.INSTANCE; }
+    private static final class InstanceHolder {
+        static final NetworkManager INSTANCE = new NetworkManager();
+    }
 
+    public static NetworkManager getInstance() {
+        return NetworkManager.InstanceHolder.INSTANCE;
+    }
+
+    public static ChessPieceColour initialColor;
     static ExecutorService service = Executors.newFixedThreadPool(10);
 
     public static String createSession(String name) {
         Future<String> future = service.submit(new NetworkTasks.CreateSession(name));
-        try{
+        try {
             String lobbycode = future.get();
-            Log.i("NETWORK","LobbyCode: "+lobbycode);
+            Log.i("NETWORK", "LobbyCode: " + lobbycode);
 
             return lobbycode;
-        } catch (InterruptedException | ExecutionException e){
-            Log.e("NETWORK","Couldnt get Value from Future");
+        } catch (InterruptedException | ExecutionException e) {
+            Log.e("NETWORK", "Couldnt get Value from Future");
             //Thread.currentThread().interrupt();
         }
         return null;
@@ -34,16 +42,36 @@ public class NetworkManager {
 
     public static LobbyDataObject joinSession(String lobbycode, String name) {
         Future<LobbyDataObject> future = service.submit(new NetworkTasks.JoinSession(lobbycode, name));
-        try{
+        try {
             LobbyDataObject lobbyDataObject = future.get();
-            Log.i("NETWORK","LobbyCode: "+lobbyDataObject);
+            Log.i("NETWORK", "LobbyCode: " + lobbyDataObject);
 
             return lobbyDataObject;
-        } catch (InterruptedException | ExecutionException e){
-            Log.e("NETWORK","Couldnt get Value from Future");
+        } catch (InterruptedException | ExecutionException e) {
+            Log.e("NETWORK", "Couldnt get Value from Future");
             //Thread.currentThread().interrupt();
         }
         return null;
+    }
+
+    public static void startGame(String lobbycode) {
+        Future<startGameParameters> future = service.submit(new NetworkTasks.startGame(lobbycode));
+        try {
+            startGameParameters parameters = future.get();
+            Log.i("COLOR", "COLOR: " + parameters.getInitColour());
+            NetworkManager.initialColor = parameters.getInitColour();
+        } catch (InterruptedException | ExecutionException e) {
+            Log.e("NETWORK", "Couldnt get Value from Future");
+            //Thread.currentThread().interrupt();
+        }
+    }
+
+    public static ChessPieceColour getInitialColor() {
+        return initialColor;
+    }
+
+    public static void setInitialColor(ChessPieceColour initialColor) {
+        NetworkManager.initialColor = initialColor;
     }
 
     /*public static void leaveSession() {
