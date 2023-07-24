16d25d01007ed49b9877bffe7198e70388928b14
bensuppanz <benjaminsuppanz@gmail.com>
2023-07-24 19:39:25 +0200

    added lobbycode to startGameRequest

--- app/src/main/java/com/game/chessmate/GameFiles/Networking/NetObjects/startGameRequest.java
+++ app/src/main/java/com/game/chessmate/GameFiles/Networking/NetObjects/startGameRequest.java
@@ -1,4 +1,16 @@
 package com.game.chessmate.GameFiles.Networking.NetObjects;
 
 public class startGameRequest {
+    String lobbycode;
+
+    public startGameRequest() {
+    }
+
+    public String getLobbycode() {
+        return lobbycode;
+    }
+
+    public void setLobbycode(String lobbycode) {
+        this.lobbycode = lobbycode;
+    }
 }
