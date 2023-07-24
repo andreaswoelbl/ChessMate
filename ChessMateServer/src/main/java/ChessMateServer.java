efc570bb306e3aaab39f9afae8f7b31dec689227
bensuppanz <benjaminsuppanz@gmail.com>
2023-07-24 19:45:55 +0200

    adjusted backend for cheat function

--- ChessMateServer/src/main/java/ChessMateServer.java
+++ ChessMateServer/src/main/java/ChessMateServer.java
@@ -1,24 +1,26 @@
 import NetObjects.createSessionRequest;
 import NetObjects.createSessionResponse;
 import NetObjects.joinSessionRequest;
+
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 
 import java.io.IOException;
 
-public class ChessMateServer extends Thread{
+public class ChessMateServer extends Thread {
     int TCP_PORT = 53216;//54555
     Server serverInstance;
 
-    ChessMateServer(){}
+    ChessMateServer() {
+    }
 
     @Override
     public void run() {
         start();
     }
 
-    public void start(){
+    public void start() {
         serverInstance = new Server();
         NetObjectRegistrator.register(serverInstance.getKryo());
         try {
@@ -30,11 +32,11 @@ public class ChessMateServer extends Thread{
 
         serverInstance.addListener(new Listener() {
             @Override
-            public void received (Connection con, Object o) {
+            public void received(Connection con, Object o) {
                 if (o instanceof createSessionRequest) {
                     System.out.println("[CREATE_SESSION_REQUEST]");
                     // Receive
-                    createSessionRequest request = (createSessionRequest)o;
+                    createSessionRequest request = (createSessionRequest) o;
                     System.out.println("Request Arg: " + request.getName());
                     // Process
                     Lobby lobby = new Lobby();
@@ -47,30 +49,35 @@ public class ChessMateServer extends Thread{
                 if (o instanceof joinSessionRequest) {
                     System.out.println("[JOIN_SESSION_REQUEST]");
                     // Receive
-                    joinSessionRequest request = (joinSessionRequest)o;
+                    joinSessionRequest request = (joinSessionRequest) o;
                     System.out.println("Name: " + request.getName());
                     System.out.println("LobbyCode: " + request.getLobbycode());
                     // Process
                     Lobby lobby = LobbyManager.getSessionByLobbycode(request.getLobbycode());
-                    if(lobby!=null){
-                        lobby._player2_join(con,request.getName());
+                    if (lobby != null) {
+                        lobby._player2_join(con, request.getName());
                         // Send
-                        ObjectSender.sendLobbyDataObject(con,lobby);
+                        if (lobby.cheatFuncActive) {
+                            //WHEN PLAYER PROCEEDED WITH MOVE INSTEAD OF SENSORACTIVATION
+                            lobby.cheatFuncActive = false;
+                        }
+                        lobby.cheatFuncActive = request.isCheatActivated();
+                        ObjectSender.sendLobbyDataObject(con, lobby);
                     } else {
                         //TODO lobby doesn't exist
                     }
                 }
-                System.out.println(con.toString() +"\t"+ o.toString() +"\n");
+                System.out.println(con.toString() + "\t" + o.toString() + "\n");
             }
 
             @Override
             public void connected(Connection connection) {
-                System.out.println("Connected: " +connection.toString());
+                System.out.println("Connected: " + connection.toString());
             }
 
             @Override
             public void disconnected(Connection connection) {
-                System.out.println("Disconnected: " +connection.toString());
+                System.out.println("Disconnected: " + connection.toString());
             }
         });
     }
